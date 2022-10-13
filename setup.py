f0a9006b0ee7a2a95ea6ce24623d697e6435ce48
Gilles de Hollander <gilles.de.hollander@gmail.com>
2022-10-13 16:26:11 +0200

    Restructure package a bit; can be installed now

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='stress_risk',
+    version='0.1',
+    url='https://github.com/Gilles86/stress_risk',
+    author='Gilles de Hollander, Maike Renkert',
+    author_email='gilles.de.holander@gmail.com',
+    description='Description of my package',
+    packages=find_packages(),
+    install_requires=[],
+)
--- stress_risk/utils/__init__.py
+++ stress_risk/utils/__init__.py
@@ -251,3 +251,11 @@ class Subject(object):
         
 
 # %%
+def get_target_dir(subject, session, sourcedata, base, modality='func'):
+    target_dir = op.join(sourcedata, 'derivatives', base, f'sub-{subject}', f'ses-{session}',
+                         modality)
+
+    if not op.exists(target_dir):
+        os.makedirs(target_dir)
+
+    return target_dir
