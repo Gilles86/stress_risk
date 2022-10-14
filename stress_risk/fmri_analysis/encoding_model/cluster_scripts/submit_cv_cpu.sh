3a0a45c2f9dad89a2e808c0a6715f1044feed877
Gilles de Hollander <gilles.de.hollander@gmail.com>
2022-10-14 10:17:18 +0200

    Code to decode on the cluster

---
+++ stress_risk/fmri_analysis/encoding_model/cluster_scripts/submit_cv_cpu.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=task_fit_cv_cpu
+#SBATCH --output=/home/cluster/gdehol/logs/fit_nprf_smoothed_surf_%A-%a.txt
+#SBATCH --partition=generic
+#SBATCH --ntasks=1
+#SBATCH -c16
+#SBATCH --mem=96G
+#SBATCH --time=2:00:00
+
+# . $HOME/init_conda.sh
+# . $HOME/init_freesurfer.sh
+. $HOME/.bashrc.sh
+
+export PARTICIPANT_LABEL=$(printf "%02d" $SLURM_ARRAY_TASK_ID)
+
+source activate tf2-gpu
+python $HOME/git/stress_risk/stress_risk/fmri_analysis/encoding_model/fit_model_cv.py $PARTICIPANT_LABEL 1 --bids_folder /scratch/gdehol/ds-stressrisk --denoise
+python $HOME/git/stress_risk/stress_risk/fmri_analysis/encoding_model/fit_model_cv.py $PARTICIPANT_LABEL 1 --bids_folder /scratch/gdehol/ds-stressrisk --smoothed --denoise
