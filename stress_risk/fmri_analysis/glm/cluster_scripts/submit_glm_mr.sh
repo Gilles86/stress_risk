13b027c9f5a5ea27f3902fe807e28cf497b8650f
mrenke <maikerenkert95@gmail.com>
2022-11-08 13:26:36 +0100

    added mrenke specififc glm/cluster_scripts

---
+++ stress_risk/fmri_analysis/glm/cluster_scripts/submit_glm_mr.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --job-name=fit_st_denoise
+#SBATCH --output=/home/cluster/mrenke/logs/fit_st_denoise_%A-%a.txt
+#SBATCH --partition=generic
+#SBATCH --ntasks=1
+#SBATCH -c 16
+#SBATCH --time=45:00
+
+. $HOME/.bashrc
+
+export PARTICIPANT_LABEL=$(printf "%02d" $SLURM_ARRAY_TASK_ID)
+
+python $HOME/git/stress_risk/stress_risk/fmri_analysis/glm/fit_glm_denoise.py $PARTICIPANT_LABEL 1 --bids_folder /data/mrenke/ds-stressrisk
