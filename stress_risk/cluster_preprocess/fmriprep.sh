960b47d11c5726ff88d589b133f39fcf732cfbc5
mrenke <maikerenkert95@gmail.com>
2023-01-17 17:52:05 +0100

    new sciencecluster fmriprep script adjustment

--- stress_risk/cluster_preprocess/fmriprep.sh
+++ stress_risk/cluster_preprocess/fmriprep.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH --job-name=fmriprep
-#SBATCH --output=/home/cluster/gdehol/logs/res_fmriprep_%A-%a.txt
-#SBATCH --partition=generic
+#SBATCH --output=/home/mrenke/logs/res_fmriprep_%A-%a.txt
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=16
 #SBATCH --mem=64G
-#SBATCH --time=08:00:00
+#SBATCH --time=24:00:00
+module load singularityce/3.10.2
 export SINGULARITYENV_FS_LICENSE=$HOME/freesurfer/license.txt
 export PARTICIPANT_LABEL=$(printf "%02d" $SLURM_ARRAY_TASK_ID)
 export SINGULARITYENV_TEMPLATEFLOW_HOME=/opt/templateflow
-singularity run -B /scratch/$USER/templateflow:/opt/templateflow --cleanenv /data/$USER/containers/fmriprep-20.2.3.simg /scratch/$USER/ds-stressrisk /scratch/$USER/ds-stressrisk/derivatives participant --participant-label $PARTICIPANT_LABEL  --output-spaces MNI152NLin2009cAsym T1w fsaverage fsnative  --dummy-scans 3 --skip_bids_validation -w /scratch/$USER/workflow_folders --no-submm-recon
+singularity run -u -B /scratch/$USER/ds-stressrisk:/data -B /scratch/$USER/workflow_folders:/workflow -B /scratch/$USER/templateflow:/opt/templateflow --cleanenv /data/$USER/containers/fmriprep_20.2.3  /data /data/derivatives participant --participant-label $PARTICIPANT_LABEL  --output-spaces MNI152NLin2009cAsym T1w fsaverage fsnative  --dummy-scans 3 --skip_bids_validation -w /workflow --no-submm-recon
