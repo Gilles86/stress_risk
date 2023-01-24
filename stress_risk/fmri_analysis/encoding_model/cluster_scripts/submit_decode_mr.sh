748d5d25e924c63f8db2de99ed6d5b43a0808fb3
mrenke <maikerenkert95@gmail.com>
2023-01-24 09:43:42 +0100

    new sciencecluster2 .sh submit scripts

---
+++ stress_risk/fmri_analysis/encoding_model/cluster_scripts/submit_decode_mr.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --job-name=decode_volume
+#SBATCH --output=/home/mrenke/logs/decode_volume_%A-%a.txt
+#SBATCH --ntasks=1
+#SBATCH --mem=96G
+#SBATCH --gres gpu:1
+#SBATCH --time=1:30:00
+
+source /etc/profile.d/lmod.sh
+module load gpu cuda
+
+# . $HOME/init_conda.sh
+
+. $HOME/.bashrc.sh
+
+source activate numrefields
+
+export PARTICIPANT_LABEL=$(printf "%02d" $SLURM_ARRAY_TASK_ID)
+
+source activate tf2-gpu
+
+python $HOME/git/stress_risk/stress_risk/fmri_analysis/encoding_model/decode.py $PARTICIPANT_LABEL 1 --bids_folder /scratch/mrenke/ds-stressrisk --n_voxels 250 --mask NPC_R  --denoise
+python $HOME/git/stress_risk/stress_risk/fmri_analysis/encoding_model/decode.py $PARTICIPANT_LABEL 2 --bids_folder /scratch/mrenke/ds-stressrisk --n_voxels 250 --mask NPC_R  --denoise
--- stress_risk/fmri_analysis/glm/cluster_scripts/submit_glm_mr.sh
+++ stress_risk/fmri_analysis/glm/cluster_scripts/submit_glm_mr.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 #SBATCH --job-name=fit_st_denoise
-#SBATCH --output=/home/cluster/mrenke/logs/fit_st_denoise_%A-%a.txt
-#SBATCH --partition=generic
+#SBATCH --output=/home/mrenke/logs/fit_st_denoise_%A-%a.txt
 #SBATCH --ntasks=1
 #SBATCH -c 16
 #SBATCH --time=45:00
 
 . $HOME/.bashrc
 
+source activate numrefields
+
 export PARTICIPANT_LABEL=$(printf "%02d" $SLURM_ARRAY_TASK_ID)
 
-python $HOME/git/stress_risk/stress_risk/fmri_analysis/glm/fit_glm_denoise.py $PARTICIPANT_LABEL 1 --bids_folder /data/mrenke/ds-stressrisk
+python $HOME/git/stress_risk/stress_risk/fmri_analysis/glm/fit_glm_denoise.py $PARTICIPANT_LABEL 1 --bids_folder /scratch/mrenke/ds-stressrisk
+python $HOME/git/stress_risk/stress_risk/fmri_analysis/glm/fit_glm_denoise.py $PARTICIPANT_LABEL 2 --bids_folder /scratch/mrenke/ds-stressrisk
