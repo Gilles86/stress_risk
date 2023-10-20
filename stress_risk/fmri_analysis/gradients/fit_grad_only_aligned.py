ae0a9c4f7c87797ad3192f8e7b3086c4655a0e08
mrenke <maikerenkert95@gmail.com>
2023-10-20 11:48:07 -0700

    git grad ref-marg-aligned +app/kernel options

--- stress_risk/fmri_analysis/gradients/fit_grad_only_aligned.py
+++ stress_risk/fmri_analysis/gradients/fit_grad_only_aligned.py
@@ -9,7 +9,7 @@ from time import process_time
 
 t = process_time()
 
-def main(sub,ses,bids_folder):
+def main(sub,ses,bids_folder,ref_marg_align,kernel,approach ):
 
     t = process_time()
     source_folder = op.join(bids_folder,'derivatives','correlation_matrices')
@@ -23,16 +23,23 @@ def main(sub,ses,bids_folder):
         print('correlation matrix as to be generated first!!') 
         sys.exit()
 
-    g_ref = np.load(op.join(bids_folder,'derivatives', 'gradients','gm_av50_unfiltered.npy')) # same labeling_noParcel as cm_unfiltered
+    if ref_marg_align:
+        ref_specification = 'aligned-marg' 
+    else:
+        ref_specification = ''
 
-    g_align = GradientMaps(kernel='normalized_angle', n_components=3,approach='le', alignment='procrustes')
+    g_ref = np.load(op.join(bids_folder,'derivatives', 'gradients',f'gm_av50_unfiltered{ref_specification}.npy')) # same labeling_noParcel as cm_unfiltered
+
+    g_align = GradientMaps(kernel=kernel, n_components=3,approach=approach, alignment='procrustes')
     elapsed_time = process_time() - t
     print(f'{elapsed_time}: starting fitting')
     g_align.fit(cm,reference=g_ref)
     elapsed_time = process_time() - t
     print(f'{elapsed_time}: finished fitting')
     
-    np.save(op.join(target_folder,f'sub-{sub}_ses-{ses}_ref-p-align_fsav5_unfiltered.npy'),g_align.gradients_) # 
+    kernel_specification = f'_kernel-{kernel}'
+
+    np.save(op.join(target_folder,f'sub-{sub}_ses-{ses}_ref-p-align_fsav5_unfiltered{ref_specification}{kernel_specification}.npy'),g_align.gradients_) # 
 
 
 if __name__ == '__main__':
@@ -41,7 +48,11 @@ if __name__ == '__main__':
     parser.add_argument('subject', default=None)
     parser.add_argument('session', default=1, type=int)  
     parser.add_argument('--bids_folder', default='/data/ds-stressrisk')
+    parser.add_argument('--ref_marg_align', default=False)
+    parser.add_argument('--kernel', default=None) # 'normalized_angle',  #Kernel function. If None, only sparsify. Default is None.
+    parser.add_argument('--approach', default='dm')# Embedding approach. Default is 'dm'
+    
     #parser.add_argument('--specification', default='')
     cmd_args = parser.parse_args()
 
-    main(cmd_args.subject, cmd_args.session, cmd_args.bids_folder)
+    main(cmd_args.subject, cmd_args.session, cmd_args.bids_folder, cmd_args.ref_marg_align, cmd_args.kernel, cmd_args.approach)
