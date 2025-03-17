9db7cf92febc1635b222c993c09c966418048361
mrenke <maikerenkert95@gmail.com>
2025-03-17 12:03:23 +0100

    reg nPRFs ana

--- stress_risk/fmri_analysis/glm/cluster_scripts/submit_glm_mr.sh
+++ stress_risk/fmri_analysis/glm/cluster_scripts/submit_glm_mr.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 #SBATCH --job-name=fit_st_denoise
-#SBATCH --output=/home/mrenke/logs/stress_risk_logs/fit_st_denoise_bothSes_value_%A-%a.txt
+#SBATCH --output=/home/mrenke/logs/stress_risk_logs/fit_st_denoise_bothSes_%A-%a.txt
 #SBATCH --ntasks=1
 #SBATCH -c 16
-#SBATCH --time=2:00:00
+#SBATCH --mem=16G 
+#SBATCH --time=3:00:00
 
 . $HOME/.bashrc
 
@@ -11,5 +12,5 @@ source activate numrefields
 
 export PARTICIPANT_LABEL=$(printf "%02d" $SLURM_ARRAY_TASK_ID)
 
-python $HOME/git/stress_risk/stress_risk/fmri_analysis/glm/fit_glm_denoise.py $PARTICIPANT_LABEL 1 --bids_folder /scratch/mrenke/ds-stressrisk
-python $HOME/git/stress_risk/stress_risk/fmri_analysis/glm/fit_glm_denoise.py $PARTICIPANT_LABEL 2 --bids_folder /scratch/mrenke/ds-stressrisk
+python $HOME/git/stress_risk/stress_risk/fmri_analysis/glm/fit_glm_denoise.py $PARTICIPANT_LABEL 1 --bids_folder /shares/zne.uzh/mrenke/ds-stressrisk --smoothed
+python $HOME/git/stress_risk/stress_risk/fmri_analysis/glm/fit_glm_denoise.py $PARTICIPANT_LABEL 2 --bids_folder /shares/zne.uzh/mrenke/ds-stressrisk --smoothed
